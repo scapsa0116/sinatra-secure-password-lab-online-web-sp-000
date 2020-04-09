5a4ee17e34b696b612210bbbfb5de5a2744d13fc
Stela Capsa <stela.capsa@gmail.com>
2020-04-09 19:41:18 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,13 +17,13 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    #your code here
-  #   user = User.new(:username => params[:username], :password => params[:password])
-  # if user.save
-  # redirect "/login"
-  # else 
-  # redirect "/failure"
-  # end
+    your code here
+    user = User.new(:username => params[:username], :password => params[:password])
+  if user.save
+  redirect "/login"
+  else 
+  redirect "/failure"
+  end
   if params[:username] == "" || params[:password] == ""
       redirect to "/failure"
     else
